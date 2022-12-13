7c62d5a97f99e5e024a422bd593339c3d9ddc09e
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-13 14:58:28 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @m: the character
+ * Return: value of last digit
+ */
+int print_last_digit(int m)
+{
+	int c;
+
+	c = m % 10;
+		_putchar(c);
+		return (c);
+}
