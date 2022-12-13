78497ec33cf82fb64c74fc30ca0a4c438123e87e
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-13 01:36:12 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	void print_alphabet(void);
+
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+
+	putchar('\n');
+	return (0);
+}
