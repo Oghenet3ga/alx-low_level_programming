7cb2ca04716ef30664749d181d8c03d0584d1888
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-13 17:37:18 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds integers
+ * @a: the character
+ * @b: the character
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	int c = a + b;
+
+	_putchar(c);
+	return (c);
+}
