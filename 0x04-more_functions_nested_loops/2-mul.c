e4c81e22d48603847f690174f63b3fc74b89d033
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-15 07:49:55 -0800

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the character
+ * @b: the character
+ * Return: two integers
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
