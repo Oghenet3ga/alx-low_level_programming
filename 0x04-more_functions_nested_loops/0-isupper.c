19a64b669643c642c29fad4887acdc03101c55fe
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-15 06:33:07 -0800

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character
+ * Return: 1 if c is uppercase, else 0
+ */
+int _isupper(int c)
+{
+	c = 'A';
+
+		if (c >= 'A', c <= 'Z', c++)
+			return (1);
+		else
+			return (0);
+}
