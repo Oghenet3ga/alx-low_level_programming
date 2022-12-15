416518a9b5dc8ea486770f216b8323b7bc622bea
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-15 15:45:38 -0800

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: the character
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+
+	_putchar('\n');
+}
