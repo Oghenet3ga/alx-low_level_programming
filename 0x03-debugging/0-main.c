5a244a96f2f8a6d8812fc62f808fcb0fd0c7b1e8
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-14 23:16:05 -0800

    Positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * * positive_or_negative - tests function that prints if integer is positive
+ *
+ * * Return: 0
+ * * @i: the character
+ *
+ */
+void positive_or_negative(int i)
+{
+
+			 i = 0;
+
+			 positive_or_negative(i);
+
+
+}
