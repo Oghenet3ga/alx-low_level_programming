b20ea27e181de1a43674d9d3086a7f1d3e094c52
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-19 02:50:53 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: the character
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0' ; len++)
+		return (len);
+}
