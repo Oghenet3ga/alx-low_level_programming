3f20ac4d3d01f4c04838b9a74c1c9bbb2644a7c5
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-21 12:28:58 -0800

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,66 +1,51 @@
 #include "main.h"
 
 /**
-*cap_string - capitalizes all words of a string.
- * @s: string.
- * Return: string.
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
+ * Return: capitalizes letters
  */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 
 {
-
 	int i;
-
-
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	i = 0;
 
-	while (s[i] != '\0')
+
+
+	while (str[i] != '\0')
 
 	{
 
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
-
-		|| s[i - 1] == '\t' || s[i - 1] == ','
-
-		|| s[i - 1] == ';' || s[i - 1] == '!'
-
-		|| s[i - 1] == '?' || s[i - 1] == '"'
-
-		|| s[i - 1] == '(' || s[i - 1] == ')'
-
-		|| s[i - 1] == '{' || s[i - 1] == '}'
-
-		|| s[i - 1] == '.')
-
-		&& (s[i] >= 'a' && s[i] <= 'z'))
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
 
 		{
 
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 
 		}
 
-		else if ((s[0] >= 97 && s[0] <= 122))
+		j = 0;
+
+		while (c[j] != '\0')
 
 		{
 
-			s[0] = s[0] - 32;
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 
+			{
+
+				str[i + 1] = str[i + 1] - 32;
+
+			}
+
+			j++;
 		}
-		else
-
-		{
-
-			s[i] = s[i];
-
+			i++;
 		}
-
-		i++;
-
-	}
-
-	return (s);
-
+		return (str);
 }
