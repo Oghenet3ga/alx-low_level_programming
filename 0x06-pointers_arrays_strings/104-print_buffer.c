b1873d66584a3f729df30f27d91bc2726ee6c894
Oghenetega Fregene <oghenetega.f@yahoo.com>
2022-12-21 12:55:24 -0800

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
@@ -8,38 +7,22 @@
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
-
 void print_buffer(char *b, int size)
-
 {
-
 	int byte, index;
 
-
-
 	for (byte = 0; byte < size; byte += 10)
-
 	{
-
 		printf("%08x: ", byte);
 
-
-
 		for (index = 0; index < 10; index++)
-
 		{
-
 			if ((index + byte) >= size)
 
 				printf("  ");
 
-
-
 			else
-
 				printf("%02x", *(b + index + byte));
-
-
 
 			if ((index % 2) != 0 && index != 0)
 
@@ -47,48 +30,20 @@ void print_buffer(char *b, int size)
 
 		}
 
-
-
 		for (index = 0; index < 10; index++)
-
 		{
-
 			if ((index + byte) >= size)
-
 				break;
-
-
-
 			else if (*(b + index + byte) >= 31 &&
-
 				 *(b + index + byte) <= 126)
-
 				printf("%c", *(b + index + byte));
-
-
-
 			else
-
 				printf(".");
-
 		}
-
-
-
 		if (byte >= size)
-
 			continue;
-
-
-
 		printf("\n");
-
 	}
-
-
-
 	if (size <= 0)
-
 		printf("\n");
-
 }
