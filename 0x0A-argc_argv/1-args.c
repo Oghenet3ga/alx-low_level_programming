893539c1b7ce7423e1492e13c4e42d243bc6921b
OghenetegaFregene <oghenetega.f@yahoo.com>
2023-01-06 09:41:02 -0800

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of argemts entered on the cl
+ * @argv: array of strings of argc
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
